dd5f3e845d1891300884e6edb6943f195b97742c
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-02-07 13:11:29 +0100

    runnable interface thread sus amogus

--- Threads/src/com/company/Main.java
+++ Threads/src/com/company/Main.java
@@ -3,24 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        UserThread userThread = new UserThread("user thread");
+        UserThread userThread = new UserThread("user thread 0");
+        userThread.start();
         int i = 0;
         while (i < 4) {
             if (!userThread.isAlive()) {
                 i++;
                 userThread.interrupt();
-                userThread = new UserThread("user thread");
+                userThread = new UserThread("user thread " + i);
                 userThread.start();
             }
         }
 
         i = 0;
-        IntRunabl intRunabl = new IntRunabl();
+        IntRunabl intRunabl = new IntRunabl("thread " + i);
         Thread intThread = new Thread(intRunabl);
+        intThread.start();
         while (i < 4) {
             if (!intThread.isAlive()) {
                 i++;
                 intThread.interrupt();
+                intRunabl = new IntRunabl("thread " + i);
                 intThread = new Thread(intRunabl);
                 intThread.start();
             }
