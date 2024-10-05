480401c38f3f664d52b263dec3fb3d792627d112
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-10-05 10:29:27 +0200

    rinominato variabili, fatte query

--- AlgebraRelazionale/src/Main.java
+++ AlgebraRelazionale/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -14,8 +16,8 @@ public class Main {
 
         System.out.println("\nSelection id=0\n" + Relation.selection(persone, "id", "0"));
 
-        ArrayList<String> keys = new ArrayList<>(Arrays.asList(new String[] { "id_utente", "nome", "penna" }));
-        System.out.println("\nProjection keys={id, nome, penna}\n" + Relation.projection(persone, keys));
+        ArrayList<String> Pkeys = new ArrayList<>(Arrays.asList(new String[] { "id_utente", "nome", "penna" }));
+        System.out.println("\nProjection keys={id, nome, penna}\n" + Relation.projection(persone, Pkeys));
 
         System.out.println("\nUnion between persone1 and persone2\n" + Relation.union(persone, persone2));
 
@@ -25,18 +27,29 @@ public class Main {
                 "\nCartesian product between ordini and prodotti\n" + Relation.cartesianProduct(ordini, prodotti));
 
         ArrayList<String> junctionField = new ArrayList<>(Arrays.asList(new String[] { "id_prodotto" }));
-        Relation ordProds = Relation.junction(ordini, prodotti, junctionField);
-        System.out.println("\nJoin between ordini and prodotti\n" + ordProds);
+        Relation JordiniProdottiID_Prodotto = Relation.junction(ordini, prodotti, junctionField);
+        System.out.println("\nJoin between ordini and prodotti\n" + JordiniProdottiID_Prodotto);
 
-        doPriceQueries(ordProds);
+        doPriceQueries(JordiniProdottiID_Prodotto);
 
-        keys = new ArrayList<>(Arrays.asList(new String[] { "id_utente", "prezzo_unitario" }));
-        Relation projProdsWUsrIds = Relation.projection(ordProds, keys);
+        Pkeys = new ArrayList<>(Arrays.asList(new String[] { "id_utente", "prezzo_unitario" }));
+        Relation PprodottiID_Utente = Relation.projection(JordiniProdottiID_Prodotto, Pkeys);
 
         junctionField = new ArrayList<>(Arrays.asList(new String[] { "id_utente" }));
-        Relation ordProdsUsers = Relation.junction(projProdsWUsrIds, persone, junctionField);
+        Relation JordiniProdottiID_Utente = Relation.junction(PprodottiID_Utente, persone, junctionField);
         
-        System.out.println(ordProdsUsers);
+        int keyPrice = JordiniProdottiID_Utente.keyIndex("prezzo_unitario");
+
+        ArrayList<Integer> prices = new ArrayList<>();
+
+        for (Row row : JordiniProdottiID_Utente.getRows()) {
+            prices.add(Integer.parseInt(row.getValue(keyPrice)));
+        }
+
+        int maxPrice = Collections.max(prices);
+
+        Relation SordiniProdottiID_UtentePrezzo_Unitario = Relation.selection(JordiniProdottiID_Utente, "prezzo_unitario", maxPrice + "");
+        System.out.println("\nUsers who bought the highest priced product\n" + SordiniProdottiID_UtentePrezzo_Unitario);
     }
 
     private static void doPriceQueries(Relation relation) {
