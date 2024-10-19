0498871f2ab8da90475056f4a008ad7fb9bbfc76
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-10-19 11:53:53 +0200

    sqlquery insert & get

---
+++ SQLQueriesTool/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Query.get();
+        Query.insert(11, "'lolla'", "'pp'", null, "'qualcosa@ak.com'", "'lalalla'", "'idk'", "'MA'", 0);
+    }
+}
